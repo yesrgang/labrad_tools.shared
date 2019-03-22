7f74134ef345b8052791654d066d1937a8eb05ad
SrQ <yesrgang@gmail.com>
2019-03-22 02:40:46 -0600

    restructure elliptec devices

--- elliptec/proxy.py
+++
@@ -1,7 +0,0 @@
-from ello import ELLO
-from serial_server.proxy import SerialProxy
-
-class ELLOProxy(ELLO):
-    def __init__(self, serial_server, serial_port=None, address=0):
-        serial = SerialProxy(serial_server)
-        super(ELLOProxy, self).__init__(serial_port, address)
