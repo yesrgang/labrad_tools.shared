83c141942f06eb43b135ace5c37b19f3c9007fb5
ross hutson <rosshutson@gmail.com>
2019-03-25 15:48:12 -0600

    .

--- current_controller3/clients/default.py
+++ current_controller3/clients/default.py
@@ -5,6 +5,7 @@ import sys
 from PyQt4 import QtGui, QtCore, Qt
 from PyQt4.QtCore import pyqtSignal 
 from twisted.internet.defer import inlineCallbacks
+from twisted.internet import defer
 
 from client_tools.connection import connection
 
@@ -16,31 +17,25 @@ class ParameterLabel(QtGui.QLabel):
     
 
 class CurrentControllerClient(QtGui.QGroupBox):
+    name = None
     mouseHover = pyqtSignal(bool)
     state = None
-    qt_style = 'Gtk+'
+#    qt_style = 'Gtk+'
     i = 0
+    DeviceProxy = None
+    current_stepsize = 0.0001
     
-    def __init__(self, reactor, cxn=None):
+    def __init__(self, reactor):
         QtGui.QDialog.__init__(self)
         self.reactor = reactor
-        self.cxn = cxn
-        self.connect()
+        reactor.callInThread(self.initialize)
 
-    @inlineCallbacks
-    def connect(self):
-        try:
-            if self.cxn is None:
-                self.cxn = connection()
-                cname = '{} - {} - client'.format(self.servername, self.name)
-                yield self.cxn.connect(name=cname)
-            yield self.getDeviceInfo()
-            self.populateGUI()
-            yield self.connectSignals()
-            yield self.requestValues()
-        except Exception, e:
-            print e
-            raise
+    def initialize(self):
+        import labrad
+        cxn = labrad.connect(name=self.name)
+        self.device = self.DeviceProxy(cxn)
+        self.reactor.callFromThread(self.populateGUI)
+        self.reactor.callFromThread(self.connectSignals)
 
     @inlineCallbacks
     def getDeviceInfo(self):
@@ -58,7 +53,7 @@ class CurrentControllerClient(QtGui.QGroupBox):
         self.current_label = ParameterLabel('Current [A]: ')
         self.current_box = QtGui.QDoubleSpinBox()
         self.current_box.setKeyboardTracking(False)
-        self.current_box.setRange(*self.current_range)
+        self.current_box.setRange(*self.device.current_range)
         self.current_box.setSingleStep(self.current_stepsize)
         self.current_box.setDecimals(abs(int(np.floor(np.log10(self.current_stepsize)))))
         self.current_box.setAccelerated(True)
@@ -76,113 +71,98 @@ class CurrentControllerClient(QtGui.QGroupBox):
         self.layout.addWidget(self.power_box, 3, 1)
         self.setLayout(self.layout)
         self.setFixedSize(200, 120)
+    
+        self.update_displays()
 
-    @inlineCallbacks
+    def update_displays(self):
+        self.reactor.callInThread(self._update_displays) 
+
+    def _update_displays(self, c=None):
+        self.get_state()
+        self.get_current()
+        self.get_power()
+    
+    def get_state(self):
+        self.reactor.callInThread(self._get_state)
+
+    def _get_state(self):
+        state = self.device.state
+        self.reactor.callFromThread(self.display_state, state)
+
+    def display_state(self, state):
+        if state:
+            self.state_button.setChecked(1)
+            self.state_button.setText('On')
+        else:
+            self.state_button.setChecked(0)
+            self.state_button.setText('Off')
+   
+    def get_current(self):
+        self.reactor.callInThread(self._get_current)
+
+    def _get_current(self):
+        current = self.device.current
+        self.reactor.callFromThread(self.display_current, current)
+
+    def display_current(self, current):
+        self.current_box.setValue(current)
+    
+    def get_power(self):
+        self.reactor.callInThread(self._get_power)
+    
+    def _get_power(self):
+        power = self.device.power
+        self.reactor.callFromThread(self.display_power, power)
+
+    def display_power(self, power):
+        self.power_box.setValue(power * 1e3)
+
     def connectSignals(self):
         self.hasNewState = False
         self.hasNewCurrent = False
         self.hasNewPower = False
-        self.update_id = np.random.randint(0, 2**31 - 1)
-        server = yield self.cxn.get_server(self.servername)
-        yield server.signal__update(self.update_id)
-        yield server.addListener(listener=self.receive_update, source=None, 
-                                 ID=self.update_id)
-        yield self.cxn.add_on_connect(self.servername, self.reinitialize)
-        yield self.cxn.add_on_disconnect(self.servername, self.disable)
-
+        
         self.state_button.released.connect(self.onNewState)
         self.current_box.valueChanged.connect(self.onNewCurrent)
         
-        #self.setMouseTracking(True)
-        #self.mouseHover.connect(self.requestValues)
-        self.power_label.clicked.connect(self.requestPower)
+        self.power_label.clicked.connect(self.update_displays)
         
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.writeValues)
         self.timer.start(self.update_time)
 
-    @inlineCallbacks
-    def requestValues(self, c=None):
-        yield self.requestState()
-        yield self.requestCurrent()
-        yield self.requestPower()
-
-    @inlineCallbacks
-    def requestState(self, c=None):
-        server = yield self.cxn.get_server(self.servername)
-        request = {self.name: None}
-        yield server.states(json.dumps(request))
-    
-    @inlineCallbacks
-    def requestCurrent(self, c=None):
-        server = yield self.cxn.get_server(self.servername)
-        request = {self.name: None}
-        yield server.currents(json.dumps(request))
-    
-    @inlineCallbacks
-    def requestPower(self, c=None):
-        server = yield self.cxn.get_server(self.servername)
-        request = {self.name: None}
-        yield server.powers(json.dumps(request))
-
-#    @inlineCallbacks
-#    def requestValues(self, c=None):
-#        server = yield self.cxn.get_server(self.servername)
-#        for parameter in self.update_parameters:
-#            yield getattr(server, parameter)()
-# 
-    def receive_update(self, c, signal_json):
-        print 'update', self.i
-        self.i += 1
-        self.free = False
-        signal = json.loads(signal_json)
-        for message_type, message in signal.items():
-            device_message = message.get(self.name)
-            if (message_type == 'states') and (device_message is not None):
-                if device_message:
-                    self.state_button.setChecked(1)
-                    self.state_button.setText('On')
-                    self.state = True
-                else:
-                    self.state_button.setChecked(0)
-                    self.state_button.setText('Off')
-                    self.state = False
-            if (message_type == 'currents') and (device_message is not None):
-                self.current_box.setValue(device_message)
-            if (message_type == 'powers') and (device_message is not None):
-                self.power_box.setValue(device_message * 1e3)
-        self.free = True
-    
-    @inlineCallbacks
     def onNewState(self):
-        if self.free:
-            server = yield self.cxn.get_server(self.servername)
-            request = {self.name: None}
-            response = yield server.states(json.dumps(request))
-            server_state = json.loads(response)[self.name]
-            if server_state == self.state:
-                request = {self.name: None}
-                if self.state:
-                    yield server.shutdown(json.dumps(request))
-                else:
-                    yield server.warmup(json.dumps(request))
+        if self.state_button.isChecked():
+            self.reactor.callInThread(self.set_state, False)
+            self.display_state(False)
+        else:
+            self.reactor.callInThread(self.set_state, True)
+            self.display_state(True)
 
     def onNewCurrent(self):
-        if self.free:
-            self.hasNewCurrent = True
+        self.hasNewCurrent = True
    
     def onNewPower(self):
         pass
 
-    @inlineCallbacks
     def writeValues(self):
         if self.hasNewCurrent:
+            current = self.current_box.value()
+            self.set_current(current)
             self.hasNewCurrent = False
-            server = yield self.cxn.get_server(self.servername)
-            request = {self.name: self.current_box.value()}
-            yield server.currents(json.dumps(request))
-            request = {self.name: None}
-            yield self.requestPower()
+            self.get_power()
+
+    def set_current(self, current):
+        self.reactor.callInThread(self._set_current, current)
+
+    def _set_current(self, current):
+        self.device.current = current
+
+    def set_state(self, state):
+        self.reactor.callInThread(self._set_state, state)
+    
+    def _set_state(self, state):
+        self.device.state = state
 
     def enterEvent(self, c):
         self.mouseHover.emit(True)
@@ -193,13 +173,7 @@ class CurrentControllerClient(QtGui.QGroupBox):
     def disable(self):
         self.setDisabled(True)
 
-    @inlineCallbacks
     def closeEvent(self, x):
-        server = yield self.cxn.get_server(self.servername)
-        yield server.signal__update(self.update_id)
-        yield server.removeListener(listener=self.receive_update, source=None, 
-                                 ID=self.update_id)
-        x.accept()
         self.reactor.stop()
 
 class MultipleClientContainer(QtGui.QWidget):
@@ -228,3 +202,23 @@ class MultipleClientContainer(QtGui.QWidget):
 
     def closeEvent(self, x):
         self.reactor.stop()
+
+#if __name__ == '__main__':
+#    from current_controller3.devices.zs import DeviceProxy as ZSProxy
+#
+#    class ZSClient(CurrentControllerClient):
+#        name = 'zs'
+#        update_time = 200
+#        DeviceProxy = ZSProxy
+#    
+#    from PyQt4 import QtGui
+#    app = QtGui.QApplication([])
+#    from client_tools import qt4reactor 
+#    qt4reactor.install()
+#    from twisted.internet import reactor
+#
+#    widget = ZSClient(reactor)
+#    widget.show()
+#    reactor.run()
+#
+#
