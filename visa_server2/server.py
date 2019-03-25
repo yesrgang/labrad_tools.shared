f882636d0a97a95b4f6f855481edfa49fead7abe
yesrgang <yesrgang@gmail.com>
2019-03-25 14:06:08 -0600

    .

--- visa_server2/server.py
+++ visa_server2/server.py
@@ -16,6 +16,7 @@
 #### END NODE INFO
 #"""
 import visa
+import json
 
 from labrad.server import setting
 
@@ -33,36 +34,47 @@ class VisaServer(ThreadedServer):
     def list_resources(self, c, query='?*::INSTR'):
         return self._rm.list_resources()
     
-    @setting(11, resource_name='s', mode='i')
-    def control_ren(self, c, resource_name, mode):
-        inst = self._rm.open_resource(resource_name)
+    @setting(11, resource_name='s', resource_kwargs_json='s', mode='i')
+    def control_ren(self, c, resource_name, resource_kwargs_json, mode):
+        resource_kwargs = json.loads(resource_kwargs_json)
+        inst = self._rm.open_resource(resource_name, **resource_kwargs)
         inst.control_ren(mode)
     
-    @setting(12, resource_name='s', termination='s', encoding='s', returns='s')
-    def read(self, c, resource_name, termination=None, encoding=None):
-        inst = self._rm.open_resource(resource_name)
+    @setting(12, resource_name='s', resource_kwargs_json='s', 
+             termination=['s', '?'], encoding=['s', '?'], returns='s')
+    def read(self, c, resource_name, resource_kwargs_json, termination, 
+             encoding):
+        resource_kwargs = json.loads(resource_kwargs_json)
+        inst = self._rm.open_resource(resource_name, **resource_kwargs)
         return inst.read(termination, encoding)
     
-    @setting(13, resource_name='s', returns='v')
-    def get_timeout(self, c, resource_name, timeout=None):
-        inst = self._rm.open_resource(resource_name)
+    @setting(13, resource_name='s', resource_kwargs_json='s', returns='v')
+    def get_timeout(self, c, resource_name, resource_kwargs_json):
+        resource_kwargs = json.loads(resource_kwargs_json)
+        inst = self._rm.open_resource(resource_name, **resource_kwargs)
         return inst.timeout
 
-    @setting(14, resource_name='s', timeout='v')
-    def set_timeout(self, c, resource_name, timeout):
-        inst = self._rm.open_resource(resource_name)
+    @setting(14, resource_name='s', resource_kwargs_json='s', 
+             timeout=['v', '?'])
+    def set_timeout(self, c, resource_name, resource_kwargs_json, timeout):
+        resource_kwargs = json.loads(resource_kwargs_json)
+        inst = self._rm.open_resource(resource_name, **resource_kwargs)
         inst.timeout = timeout
     
-    @setting(15, resource_name='s', message='s', delay='v', returns='s')
-    def query(self, c, resource_name, message, delay=None):
-        inst = self._rm.open_resource(resource_name)
+    @setting(15, resource_name='s', resource_kwargs_json='s', message='s', 
+             delay=['v', '?'], returns='s')
+    def query(self, c, resource_name, resource_kwargs_json, message, delay):
+        resource_kwargs = json.loads(resource_kwargs_json)
+        inst = self._rm.open_resource(resource_name, **resource_kwargs)
         return inst.query(message, delay)
 
-    @setting(16, resource_name='s', message='s', termination='s', encoding='s',
-             returns='i')
-    def write(self, c, resource_name, message, termination=None, encoding=None):
-        inst = self._rm.open_resource(resource_name)
-        return inst.write(message, termination, encoding)
+    @setting(16, resource_name='s', resource_kwargs_json='s', message='s', 
+             termination=['s', '?'], encoding=['s', '?'], returns='i')
+    def write(self, c, resource_name, resource_kwargs_json, message, 
+              termination, encoding):
+        resource_kwargs = json.loads(resource_kwargs_json)
+        inst = self._rm.open_resource(resource_name, **resource_kwargs)
+        return inst.write(message, termination, encoding)[0]
 
 Server = VisaServer
 
