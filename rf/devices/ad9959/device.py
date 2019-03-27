5daab9753793413e15d8de813e525cfcd03ce655
SrQ <yesrgang@gmail.com>
2019-03-26 21:13:33 -0600

    hardware disable auto-reset

--- rf/devices/ad9959/device.py
+++ rf/devices/ad9959/device.py
@@ -51,9 +51,13 @@ class AD9959(DefaultDevice):
         ser.baudrate = self.serial_baudrate
         self.ser = ser
 
-        time.sleep(0.2)
-        reactor.callInThread(self.delayed_call, 5, self.set_frequency, self.default_frequency)
-        reactor.callInThread(self.delayed_call, 5.1, self.set_frequency, self.default_frequency)
+        self.set_frequency(self.default_frequency)
+        self.set_amplitude(self.default_amplitude)
+
+#        time.sleep(0.2)
+#        reactor.callInThread(self.delayed_call, 5, self.set_frequency, self.default_frequency)
+#        reactor.callInThread(self.delayed_call, 5.1, self.set_frequency, self.default_frequency)
+#        reactor.callInThread(self.delayed_call, 5.2, self.set_amplitude, self.default_amplitude)
     
     def delayed_call(self, delay, func, *args):
         time.sleep(delay)
