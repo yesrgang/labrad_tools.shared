5b385cedc1a7528e348cba548d3b3e7705e80d0b
srgang <srgang@yesr9.colorado.edu>
2019-04-02 02:09:58 -0600

    fix ok server

--- ok_server2/server.py
+++ ok_server2/server.py
@@ -22,74 +22,85 @@ from server_tools.threaded_server import ThreadedServer
 
 
 class OKServer(ThreadedServer):
-    name = '%LABRADNODE%_ok'
+    name = '%LABRADNODE%_ok2'
 
-    def _get_xem(self, realm='', serial=None):
-        #devices = ok.okCFrontPanelDevices(realm=realm)
-        devices = ok.okCFrontPanelDevices()
-        return devices.GetSerial(serial)
+    def initServer(self):
+        self._open_xems = {}
+
+    def _get_xem(self, serial=None):
+        if serial not in self._open_xems:
+            devices = ok.okCFrontPanelDevices()
+            self._open_xems[serial] = devices.Open(serial)
+        return self._open_xems[serial]
+
+    @setting(10, serial='s')
+    def close(self, c, serial):
+        xem = self._get_xem(serial)
+        xem.Close()
+        del self._open_xems[serial]
     
-    @setting(10, realm='s', serial=['s', '?'], strFilename='s')
-    def configure_fpga(self, c, realm, serial, strFilename):
-        xem = self._get_xem(realm, serial)
+    @setting(11, serial='s', strFilename='s')
+    def configure_fpga(self, c, serial, strFilename):
+        xem = self._get_xem(serial)
         xem.ConfigureFPGA(strFilename)
 
-    @setting(11, realm='s')
-    def get_count(self, c, realm):
-        #devices = ok.okCFrontPanelDevices(realm=realm)
+    @setting(12)
+    def get_count(self, c):
         devices = ok.okCFrontPanelDevices()
         return devices.GetCount()
 
-    @setting(12, realm='s', num='i')
-    def get_serial(self, c, realm, num):
-        #devices = ok.okCFrontPanelDevices(realm=realm)
+    @setting(13, num='i')
+    def get_serial(self, c, num):
         devices = ok.okCFrontPanelDevices()
         return devices.GetSerial(num)
     
-    @setting(13, realm='s', serial=['s', '?'], epAddr='i')
-    def get_wire_out_value(self, c, realm, serial, epAddr):
-        xem = self._get_xem(realm, serial)
+    @setting(14, serial='s', epAddr='i')
+    def get_wire_out_value(self, c, serial, epAddr):
+        xem = self._get_xem(serial)
         return xem.GetWireOutValue(epAddr)
     
-    @setting(14, realm='s', serial=['s', '?'], epAddr='i', mask='i')
-    def is_triggered(self, c, realm, serial, epAddr, mask):
-        xem = self._get_xem(realm, serial)
+    @setting(15, serial='s', epAddr='i', mask='w')
+    def is_triggered(self, c, serial, epAddr, mask):
+        xem = self._get_xem(serial)
         return xem.IsTriggered(epAddr, mask)
     
-    @setting(15, realm='s', serial=['s', '?'])
-    def open(self, c, realm, serial):
-        #devices = ok.okCFrontPanelDevices(realm=realm)
+    @setting(16, serial='s')
+    def open(self, c, serial=''):
         devices = ok.okCFrontPanelDevices()
         xem = devices.Open(serial)
+        serial = xem.GetSerialNumber()
+        self._open_xems[serial] = xem
+        return serial
 
-    @setting(16, serial=['s', '?'])
+    @setting(17, serial='s')
     def open_by_serial(self, c, serial):
         fp = ok.okCFrontPanel()
         xem = fp.OpenBySerial(serial)
+        self._open_xems[serial] = serial
     
-    @setting(17, realm='s', serial=['s', '?'], epAddr='i', val='i', mask='i')
-    def set_wire_in_value(self, c, realm, serial, epAddr, val, mask):
-        xem = self._get_xem(realm, serial)
+    @setting(18, serial='s', epAddr='i', val='i', mask='w')
+    def set_wire_in_value(self, c, serial, epAddr, val, mask):
+        xem = self._get_xem(serial)
         xem.SetWireInValue(epAddr, val, mask)
     
-    @setting(18, realm='s', serial=['s', '?'])
-    def update_trigger_outs(self, c, realm, serial):
-        xem = self._get_xem(realm, serial)
+    @setting(19, serial='s')
+    def update_trigger_outs(self, c, serial):
+        xem = self._get_xem(erial)
         xem.UpdateTriggerOuts()
     
-    @setting(19, realm='s', serial=['s', '?'])
-    def update_wire_ins(self, c, realm, serial):
-        xem = self._get_xem(realm, serial)
+    @setting(20, serial='s')
+    def update_wire_ins(self, c, serial):
+        xem = self._get_xem(serial)
         xem.UpdateWireIns()
     
-    @setting(20, realm='s', serial=['s', '?'])
-    def update_wire_outs(self, c, realm, serial):
-        xem = self._get_xem(realm, serial)
+    @setting(21, serial='s')
+    def update_wire_outs(self, c, serial):
+        xem = self._get_xem(serial)
         xem.UpdateWireOuts()
     
-    @setting(21, realm='s', serial='s', epAddr='i', data='*y')
-    def write_to_pipe_in(self, c, realm, serial, epAddr, data):
-        xem = self._get_xem(realm, serial)
+    @setting(22, serial='s', epAddr='i', data='*y')
+    def write_to_pipe_in(self, c, serial, epAddr, data):
+        xem = self._get_xem(serial)
         xem.WriteToPipeIn(epAddr, bytearray(data))
 
 Server = OKServer
