76b6af4b07383c53ee362f29c4edd42eae0f9a23
yesrgang <yesrgang@gmail.com>
2019-04-16 01:35:44 -0600

    .

--- current_controller3/devices/ldc50.py
+++ current_controller3/devices/ldc50.py
@@ -1,21 +1,27 @@
 import socket
+import time
 
 class LDC50(object):
     socket_address = None
     _current_range = (0.0, 153.0)
     _relock_stepsize = 0.001
+    _relock_duration = 2
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    @property
-    def current(self):
+    def _get_socket(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
         s.connect(self.socket_address)
         s.send('\n')
         s.send('ULOC 1\n')
+        return s
+
+    @property
+    def current(self):
+        s = self._get_socket()
         s.send('RILD?\n')
         response = s.recv(1024)
         s.close()
@@ -25,21 +31,13 @@ class LDC50(object):
     @current.setter
     def current(self, current_a):
         current_ma = current_a * 1e3
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
-        s.connect(self.socket_address)
-        s.send('\n')
-        s.send('ULOC 1\n')
+        s = self._get_socket()
         s.send('SILD {}\n'.format(current_ma))
         s.close()
 
     @property
     def power(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
-        s.connect(self.socket_address)
-        s.send('\n')
-        s.send('ULOC 1\n')
+        s = self._get_socket()
         s.send('RWPD?\n')
         response = s.recv(1024)
         s.close()
@@ -49,27 +47,20 @@ class LDC50(object):
     def relock(self):
         current = self.current
         self.current = current + self._relock_stepsize
+        time.sleep(self._relock_duration)
         self.current = current
 
     @property
     def state(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
-        s.connect(self.socket_address)
-        s.send('\n')
-        s.send('ULOC 1\n')
+        s = self._get_socket()
         s.send('LDON?\n')
         response = s.recv(1024)
         s.close()
-        return float(response.strip())
+        return bool(response.strip())
 
     @state.setter
     def state(self, state):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
-        s.connect(self.socket_address)
-        s.send('\n')
-        s.send('ULOC 1\n')
+        s = self._get_socket()
         if state:
             s.send('LDON ON\n')
         else:
